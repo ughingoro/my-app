15bf4d63aafd7ecd366c75a6bd75c6d1ace72389
ughingoro <ughingoro@gmail.com>
2020-07-12 22:40:25 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-app
+ first app in react
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hellow World this is a sprit
+    </div>
+  );
+}
+
+export default App;
